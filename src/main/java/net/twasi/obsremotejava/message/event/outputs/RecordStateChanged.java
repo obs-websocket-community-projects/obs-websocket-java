51fe089a92971335eee7e0d80fe1e7554cbc7d29
TinaTiel <contact@tinatiel.com>
2021-06-12 21:32:01 +0200

    Added call to super on toString so that RecordStateChanged toString is meaningful

--- src/main/java/net/twasi/obsremotejava/message/event/outputs/RecordStateChanged.java
+++ src/main/java/net/twasi/obsremotejava/message/event/outputs/RecordStateChanged.java
@@ -2,7 +2,7 @@ package net.twasi.obsremotejava.message.event.outputs;
 
 import lombok.ToString;
 
-@ToString
+@ToString(callSuper = true)
 public class RecordStateChanged extends OutputStateChanged {
     protected RecordStateChanged() {
         super(Type.RecordStateChanged, Category.Outputs);

b8757c54b4d42b38c79d73ed72ba65620190d88e
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2021-06-13 12:58:04 +0200

    fix: GetSceneList core: Remove old Event related classes

--- src/main/java/net/twasi/obsremotejava/message/response/scenes/GetSceneList.java
+++ src/main/java/net/twasi/obsremotejava/message/response/scenes/GetSceneList.java
@@ -1,18 +1,25 @@
 package net.twasi.obsremotejava.message.response.scenes;
 
 import java.util.List;
+
+import lombok.Getter;
+import lombok.ToString;
 import net.twasi.obsremotejava.message.request.Request;
 import net.twasi.obsremotejava.message.response.RequestResponse;
 import net.twasi.obsremotejava.objects.Scene;
 
+@Getter
+@ToString(callSuper = true)
 public class GetSceneList extends RequestResponse {
-    private List<Scene> scenes;
+    private Data responseData;
 
     public GetSceneList() {
         super(Request.Type.GetSceneList);
     }
 
-    public List<Scene> getScenes() {
-        return scenes;
+    @Getter
+    @ToString
+    public static class Data {
+        private List<Scene> scenes;
     }
 }

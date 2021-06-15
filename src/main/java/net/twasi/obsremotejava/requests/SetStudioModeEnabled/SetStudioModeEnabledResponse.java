0173abc4f5a073245e7f6e1ad6d7dc999fd02511
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2021-06-15 20:29:46 +0200

    core: Delete v4.x requests implemented in 5.x

--- src/main/java/net/twasi/obsremotejava/requests/SetStudioModeEnabled/SetStudioModeEnabledResponse.java
+++
@@ -1,6 +0,0 @@
-package net.twasi.obsremotejava.requests.SetStudioModeEnabled;
-
-import net.twasi.obsremotejava.requests.ResponseBase;
-
-public class SetStudioModeEnabledResponse extends ResponseBase {
-}

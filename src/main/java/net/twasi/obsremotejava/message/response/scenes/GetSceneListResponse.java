f0418ed4a504c49a127aa3f739ff99e62bc607c1
TinaTiel <contact@tinatiel.com>
2021-06-16 19:47:03 +0200

    fix: updated scene and sceneListResponse models to match updates.

--- src/main/java/net/twasi/obsremotejava/message/response/scenes/GetSceneListResponse.java
+++ src/main/java/net/twasi/obsremotejava/message/response/scenes/GetSceneListResponse.java
@@ -2,6 +2,7 @@ package net.twasi.obsremotejava.message.response.scenes;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import net.twasi.obsremotejava.message.request.Request;
@@ -11,15 +12,19 @@ import net.twasi.obsremotejava.model.Scene;
 @Getter
 @ToString(callSuper = true)
 public class GetSceneListResponse extends RequestResponse {
+
     private Data responseData;
 
     public GetSceneListResponse() {
         super(Request.Type.GetSceneList);
     }
 
+    @AllArgsConstructor
     @Getter
     @ToString
     public static class Data {
+        private String currentProgramSceneName;
+        private String currentPreviewSceneName;
         private List<Scene> scenes;
     }
 }

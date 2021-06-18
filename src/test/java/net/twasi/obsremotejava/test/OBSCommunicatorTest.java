d25a9d145e2fca673e17cbed5b371e6faf47a2bf
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2021-06-18 15:37:36 +0200

    test: OBSCommunicatorTest.exitStartedEventTriggered

--- src/test/java/net/twasi/obsremotejava/test/OBSCommunicatorTest.java
+++ src/test/java/net/twasi/obsremotejava/test/OBSCommunicatorTest.java
@@ -1,21 +1,20 @@
 package net.twasi.obsremotejava.test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.google.gson.Gson;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 import net.twasi.obsremotejava.OBSCommunicator;
+import net.twasi.obsremotejava.message.event.Event;
+import net.twasi.obsremotejava.message.event.general.ExitStartedEvent;
 import org.junit.jupiter.api.Test;
 
 class OBSCommunicatorTest {
 
   @Test
   void invalidJsonTriggersOnErrorCallback() {
-
     // Given the communicator is listening for errors
     AtomicReference<String> actualTestResult = new AtomicReference<>();
     OBSCommunicator connector = OBSCommunicator.builder()
@@ -31,12 +30,10 @@ class OBSCommunicatorTest {
 
     // Then an error will have been received
     assertThat(actualTestResult.get()).containsIgnoringCase("Message received was not valid json");
-
   }
 
   @Test
   void deserializableButUnrecognizedObjectsTriggerError() {
-
     // Given the communicator is listening for errors
     AtomicReference<String> actualTestResult = new AtomicReference<>();
     OBSCommunicator connector = OBSCommunicator.builder()
@@ -52,12 +49,10 @@ class OBSCommunicatorTest {
 
     // Then an error will be triggered
     assertEquals("Received message was deserializable but had unknown format", actualTestResult.get());
-
   }
 
   @Test
   void validButUnrecognizedMessageTypeTriggersOnErrorCallback() {
-
     // Given the communicator is listening for errors
     AtomicReference<String> actualTestResult = new AtomicReference<>();
     OBSCommunicator connector = OBSCommunicator.builder()
@@ -76,7 +71,28 @@ class OBSCommunicatorTest {
 
     // Then an error will be triggered
     assertEquals("Received message was deserializable but had unknown format", actualTestResult.get());
+  }
 
+  @Test
+  void exitStartedEventTriggered() {
+    // Given the communicator is initialized with a ExitStartedEvent listener
+    AtomicReference<ExitStartedEvent> actualTestResult = new AtomicReference<>();
+    OBSCommunicator connector = OBSCommunicator.builder()
+            .registerEventListener(ExitStartedEvent.class, actualTestResult::set)
+      .build();
+
+    // When a valid ExitStarted JSON object is supplied
+    String eventMessage = "{\n"
+      + "\t\"messageType\": \"Event\",\n"
+      + "\t\"eventType\": \"ExitStarted\"\n"
+      + "}";
+    assertTrue(isDeserializable(eventMessage));
+    connector.onMessage(eventMessage);
+
+    // Then the event listener will be called
+    assertNotNull(actualTestResult.get());
+    // And will contain the actual ExitStartedEvent instance
+    assertEquals(actualTestResult.get().getEventType(), Event.Type.ExitStarted);
   }
 
   private boolean isDeserializable(String json) {
@@ -90,5 +106,4 @@ class OBSCommunicatorTest {
       return false;
     }
   }
-
 }

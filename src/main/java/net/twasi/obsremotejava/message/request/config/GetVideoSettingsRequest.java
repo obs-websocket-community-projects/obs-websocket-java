c4aafd520547d882764a2cc7abab112e710e3773
Pjiesco <Pjiesco@gmail.com>
2021-06-21 19:55:48 +0200

    fix: removed unnecessary javadoc

--- src/main/java/net/twasi/obsremotejava/message/request/config/GetVideoSettingsRequest.java
+++ src/main/java/net/twasi/obsremotejava/message/request/config/GetVideoSettingsRequest.java
@@ -5,9 +5,6 @@ import lombok.Getter;
 import lombok.ToString;
 import net.twasi.obsremotejava.message.request.Request;
 
-/**
- * @since 2.0.0
- */
 @Getter
 @ToString(callSuper = true)
 public class GetVideoSettingsRequest extends Request {
--- src/main/java/net/twasi/obsremotejava/message/response/general/OpenProjectorResponse.java
+++ src/main/java/net/twasi/obsremotejava/message/response/general/OpenProjectorResponse.java
@@ -5,9 +5,6 @@ import lombok.ToString;
 import net.twasi.obsremotejava.message.request.Request;
 import net.twasi.obsremotejava.message.response.RequestResponse;
 
-/**
- * @since 2.0.0
- */
 @Getter
 @ToString(callSuper = true)
 public class OpenProjectorResponse extends RequestResponse {

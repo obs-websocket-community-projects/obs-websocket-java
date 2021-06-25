3d926d65bcd4dc5e8a171f91d3ce8bee50bd2e97
Pjiesco <Pjiesco@gmail.com>
2021-06-25 21:52:48 +0200

    test: getInputKindListRequest fix: test method naming

--- src/test/java/io/obswebsocket/community/test/translator/requestSerializationTests/InputsRequestSerializationTest.java
+++ src/test/java/io/obswebsocket/community/test/translator/requestSerializationTests/InputsRequestSerializationTest.java
@@ -1,13 +1,14 @@
 package io.obswebsocket.community.test.translator.requestSerializationTests;
 
 import io.obswebsocket.community.message.request.inputs.GetInputDefaultSettingsRequest;
+import io.obswebsocket.community.message.request.inputs.GetInputKindListRequest;
 import io.obswebsocket.community.message.request.inputs.GetInputListRequest;
 import io.obswebsocket.community.test.translator.AbstractSerializationTest;
 import org.junit.jupiter.api.Test;
 
 public class InputsRequestSerializationTest extends AbstractSerializationTest {
     @Test
-    void createInputRequest() {
+    void getInputDefaultSettings() {
         GetInputDefaultSettingsRequest getInputDefaultSettingsRequest = GetInputDefaultSettingsRequest.builder()
                 .inputKind("input kind")
                 .build();
@@ -22,24 +23,6 @@ public class InputsRequestSerializationTest extends AbstractSerializationTest {
                 "}";
 
         assertSerializationAndDeserialization(json, getInputDefaultSettingsRequest);
-    }
-
-    @Test
-    void getInputKindListRequest() {
-        GetInputListRequest getInputListRequest = GetInputListRequest.builder()
-                .inputKind("input kind")
-                .build();
-
-        String json = "{\n" +
-                "\t\"requestData\": {\n" +
-                "\t\t\"inputKind\": \"input kind\"\n" +
-                "\t},\n" +
-                "\t\"requestType\": \"GetInputList\",\n" +
-                "\t\"requestId\": " + getInputListRequest.getRequestId() + ",\n" +
-                "\t\"messageType\": \"Request\"\n" +
-                "}";
-
-        assertSerializationAndDeserialization(json, getInputListRequest);
     }
 
     @Test
@@ -58,5 +41,23 @@ public class InputsRequestSerializationTest extends AbstractSerializationTest {
                 "}";
 
         assertSerializationAndDeserialization(json, getInputListRequest);
+    }
+
+    @Test
+    void getInputKindListRequest() {
+        GetInputKindListRequest getInputKindListRequest = GetInputKindListRequest.builder()
+                .unversioned(false)
+                .build();
+
+        String json = "{\n" +
+                "\t\"requestData\": {\n" +
+                "\t\t\"unversioned\": false\n" +
+                "\t},\n" +
+                "\t\"requestType\": \"GetInputKindList\",\n" +
+                "\t\"requestId\": " + getInputKindListRequest.getRequestId() + ",\n" +
+                "\t\"messageType\": \"Request\"\n" +
+                "}";
+
+        assertSerializationAndDeserialization(json, getInputKindListRequest);
     }
 }

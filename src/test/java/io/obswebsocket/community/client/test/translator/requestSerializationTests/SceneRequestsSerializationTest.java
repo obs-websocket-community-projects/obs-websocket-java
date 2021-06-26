be71aaef5e38790aa6601c9ee998c21c80ec736f
Pjiesco <Pjiesco@gmail.com>
2021-06-26 16:24:12 +0200

    test: deleteSceneTransitionOverrideRequest

--- src/test/java/io/obswebsocket/community/client/test/translator/requestSerializationTests/SceneRequestsSerializationTest.java
+++ src/test/java/io/obswebsocket/community/client/test/translator/requestSerializationTests/SceneRequestsSerializationTest.java
@@ -1,6 +1,7 @@
 package io.obswebsocket.community.client.test.translator.requestSerializationTests;
 
 import io.obswebsocket.community.client.message.request.scenes.CreateSceneRequest;
+import io.obswebsocket.community.client.message.request.scenes.DeleteSceneTransitionOverrideRequest;
 import io.obswebsocket.community.client.test.translator.AbstractSerializationTest;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,23 @@ public class SceneRequestsSerializationTest extends AbstractSerializationTest {
                 "}";
 
         assertSerializationAndDeserialization(json, createSceneRequest);
+    }
+
+    @Test
+    void deleteSceneTransitionOverrideRequest() {
+        DeleteSceneTransitionOverrideRequest deleteSceneTransitionOverrideRequest = DeleteSceneTransitionOverrideRequest.builder()
+                .sceneName("Scene name")
+                .build();
+
+        String json = "{\n" +
+                "\t\"requestData\": {\n" +
+                "\t\t\"sceneName\": \"Scene name\"\n" +
+                "\t},\n" +
+                "\t\"requestType\": \"DeleteSceneTransitionOverride\",\n" +
+                "\t\"requestId\": " + deleteSceneTransitionOverrideRequest.getRequestId() + ",\n" +
+                "\t\"messageType\": \"Request\"\n" +
+                "}";
+
+        assertSerializationAndDeserialization(json, deleteSceneTransitionOverrideRequest);
     }
 }

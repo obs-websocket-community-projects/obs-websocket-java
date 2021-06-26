d2430447cee9939748b94635e94844722ade1d4b
Pjiesco <Pjiesco@gmail.com>
2021-06-26 16:21:41 +0200

    test: createSceneRequest

---
+++ src/test/java/io/obswebsocket/community/client/test/translator/requestSerializationTests/SceneRequestsSerializationTest.java
@@ -0,0 +1,25 @@
+package io.obswebsocket.community.client.test.translator.requestSerializationTests;
+
+import io.obswebsocket.community.client.message.request.scenes.CreateSceneRequest;
+import io.obswebsocket.community.client.test.translator.AbstractSerializationTest;
+import org.junit.jupiter.api.Test;
+
+public class SceneRequestsSerializationTest extends AbstractSerializationTest {
+    @Test
+    void createSceneRequest() {
+        CreateSceneRequest createSceneRequest = CreateSceneRequest.builder()
+                .sceneName("Scene name")
+                .build();
+
+        String json = "{\n" +
+                "\t\"requestData\": {\n" +
+                "\t\t\"sceneName\": \"Scene name\"\n" +
+                "\t},\n" +
+                "\t\"requestType\": \"CreateScene\",\n" +
+                "\t\"requestId\": " + createSceneRequest.getRequestId() + ",\n" +
+                "\t\"messageType\": \"Request\"\n" +
+                "}";
+
+        assertSerializationAndDeserialization(json, createSceneRequest);
+    }
+}

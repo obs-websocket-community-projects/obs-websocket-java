8baf3ffe872d669ce4bf732cde9a0b1b774d730b
Pjiesco <Pjiesco@gmail.com>
2021-06-26 15:22:50 +0200

    test: getTransitionListRequest

--- src/test/java/io/obswebsocket/community/client/test/translator/requestSerializationTests/TransitionsRequestSerializationTest.java
+++ src/test/java/io/obswebsocket/community/client/test/translator/requestSerializationTests/TransitionsRequestSerializationTest.java
@@ -1,6 +1,7 @@
 package io.obswebsocket.community.client.test.translator.requestSerializationTests;
 
 import io.obswebsocket.community.client.message.request.transitions.GetCurrentTransitionRequest;
+import io.obswebsocket.community.client.message.request.transitions.GetTransitionListRequest;
 import io.obswebsocket.community.client.test.translator.AbstractSerializationTest;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +11,24 @@ public class TransitionsRequestSerializationTest extends AbstractSerializationTe
         GetCurrentTransitionRequest getCurrentTransitionRequest = GetCurrentTransitionRequest.builder().build();
 
         String json = "{\n" +
-                "\t\"requestType\": \"GetCurrentTransition\",\n" +
+                "\\t\"requestType\": \"GetCurrentTransition\",\n" +
                 "\t\"requestId\": " + getCurrentTransitionRequest.getRequestId() + ",\n" +
-                "\t\"messageType\": \"Request\"\n" +
+                "\\t\"messageType\": \"Request\"\n" +
                 "}";
 
         assertSerializationAndDeserialization(json, getCurrentTransitionRequest);
+    }
+
+    @Test
+    void getTransitionListRequest() {
+        GetTransitionListRequest getTransitionListRequest = GetTransitionListRequest.builder().build();
+
+        String json = "{\n" +
+                "\t\"requestType\": \"GetTransitionList\",\n" +
+                "\t\"requestId\": " + getTransitionListRequest.getRequestId() + ",\n" +
+                "\t\"messageType\": \"Request\"\n" +
+                "}";
+
+        assertSerializationAndDeserialization(json, getTransitionListRequest);
     }
 }

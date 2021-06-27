7d9984e773edf16842b61adb332ca5db1726a083
Pjiesco <Pjiesco@gmail.com>
2021-06-27 13:05:09 +0200

    test: duplicateSceneItemRequest

--- src/test/java/io/obswebsocket/community/client/test/translator/requestSerializationTests/SceneItemRequestsSerializationTest.java
+++ src/test/java/io/obswebsocket/community/client/test/translator/requestSerializationTests/SceneItemRequestsSerializationTest.java
@@ -1,6 +1,7 @@
 package io.obswebsocket.community.client.test.translator.requestSerializationTests;
 
 import io.obswebsocket.community.client.message.request.sceneItems.CreateSceneItemRequest;
+import io.obswebsocket.community.client.message.request.sceneItems.DuplicateSceneItemRequest;
 import io.obswebsocket.community.client.message.request.sources.GetSourceActiveRequest;
 import io.obswebsocket.community.client.message.request.sources.GetSourceScreenshotRequest;
 import io.obswebsocket.community.client.message.request.sources.SaveSourceScreenshotRequest;
@@ -18,12 +19,35 @@ public class SceneItemRequestsSerializationTest extends AbstractSerializationTes
         String json = "{\n" +
                 "\t\"requestData\": {\n" +
                 "\t\t\"inputName\": \"Input name\",\n" +
-                "t\t\"sceneName\": \"Scene name\"\n" +
+                "\t\t\"sceneName\": \"Scene name\"\n" +
                 "\t},\n" +
                 "\t\"requestType\": \"CreateSceneItem\",\n" +
                 "\t\"requestId\": " + createSceneItemRequest.getRequestId() + ",\n" +
-                "\t\"messageType\": \"Request\"\n";
+                "\t\"messageType\": \"Request\"\n" +
+                "}";
 
         assertSerializationAndDeserialization(json, createSceneItemRequest);
+    }
+
+    @Test
+    void duplicateSceneItemRequest() {
+        DuplicateSceneItemRequest duplicateSceneItemRequest = DuplicateSceneItemRequest.builder()
+                .sceneName("Scene name")
+                .sceneItemId(1234)
+                .destinationSceneName("new SceneItem")
+                .build();
+
+        String json = "{\n" +
+                "\t\"requestData\": {\n" +
+                "\t\t\"destinationSceneName\": \"new SceneItem\",\n" +
+                "\t\t\"sceneItemId\": 1234,\n" +
+                "\t\t\"sceneName\": \"Scene name\"\n" +
+                "\t},\n" +
+                "\t\"requestType\": \"DuplicateSceneItem\",\n" +
+                "\t\"requestId\": " + duplicateSceneItemRequest.getRequestId() + ",\n" +
+                "\t\"messageType\": \"Request\"\n" +
+                "}";
+
+        assertSerializationAndDeserialization(json, duplicateSceneItemRequest);
     }
 }

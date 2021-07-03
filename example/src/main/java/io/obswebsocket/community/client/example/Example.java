056697c9d81cdef9c6c1602a9b72fb0702b32ef3
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2021-07-03 17:09:24 +0200

    doc: Add example implementation

--- example/src/main/java/io/obswebsocket/community/client/example/Example.java
+++ example/src/main/java/io/obswebsocket/community/client/example/Example.java
@@ -3,11 +3,15 @@ package io.obswebsocket.community.client.example;
 import io.obswebsocket.community.client.OBSRemoteController;
 import io.obswebsocket.community.client.message.event.general.StudioModeStateChangedEvent;
 import io.obswebsocket.community.client.message.request.general.GetStudioModeEnabledRequest;
+import io.obswebsocket.community.client.message.response.general.GetStudioModeEnabledResponse;
+import io.obswebsocket.community.client.model.Scene;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Example {
 
   private final OBSRemoteController obsRemoteController;
-  private boolean isReconnect = false;
+  private boolean isReconnecting = false;
 
   public static void main(String[] args) {
     new Example();
@@ -30,34 +34,61 @@ public class Example {
     this.obsRemoteController.connect();
   }
 
-  private void onStudioModeStateChanged(StudioModeStateChangedEvent studioModeStateChangedEvent) {
-    System.out.printf(
-        "Studio Mode State Changed to: %B%n", studioModeStateChangedEvent.getEventData().getStudioModeEnabled());
-  }
-
   private void onReady() {
-    if (!this.isReconnect) {
+    if (!this.isReconnecting) {
       // First connection
-
-      // Send a request through a convenience method
-      this.obsRemoteController.getSceneList(System.out::println);
-
-      // Disconnect
-      this.obsRemoteController.disconnect();
-
-      // Set a flag
-      this.isReconnect = true;
-      // Reconnect
-      this.obsRemoteController.connect(); // onReady will be called another time
+      this.onFirstConnection();
     }
     else {
       // Second connection
+      this.onSecondConnection();
+    }
+  }
 
-      // Send a Request through  specific Request builder
-      this.obsRemoteController.sendRequest(GetStudioModeEnabledRequest.builder().build(), System.out::println);
+  private void onFirstConnection() {
+    // Send a request through a convenience method
+    this.obsRemoteController.getSceneList(getSceneListResponse -> {
+      if (getSceneListResponse.isSuccessful()) {
+        // Filter by isGroup
+        List<Scene> groups = getSceneListResponse.getResponseData().getScenes().stream().filter(Scene::getIsGroup).collect(Collectors.toList());
+        // Print each Scene
+        groups.forEach(System.out::println);
+      }
+
+      this.disconnectAndReconnect();
+    });
+  }
+
+  private void onSecondConnection() {
+    // Send a Request through  specific Request builder
+    this.obsRemoteController.sendRequest(GetStudioModeEnabledRequest.builder().build(), requestResponse -> {
+      if (requestResponse.isSuccessful()) {
+        GetStudioModeEnabledResponse getStudioModeEnabledResponse = (GetStudioModeEnabledResponse) requestResponse;
+        if (getStudioModeEnabledResponse.getResponseData().getStudioModeEnabled()) {
+          // Studio Mode is enabled
+        }
+        else {
+          // Studio Mode is not enabled
+        }
+      }
 
       // Disconnect
       this.obsRemoteController.disconnect();
-    }
+    });
+  }
+
+  private void disconnectAndReconnect() {
+    // Disconnect
+    this.obsRemoteController.disconnect();
+
+    // Set a flag
+    this.isReconnecting = true;
+    // Reconnect
+    this.obsRemoteController.connect(); // onReady will be called another time
+  }
+
+  private void onStudioModeStateChanged(StudioModeStateChangedEvent studioModeStateChangedEvent) {
+    System.out.printf(
+        "Studio Mode State Changed to: %B%n", studioModeStateChangedEvent.getEventData().getStudioModeEnabled());
   }
 }

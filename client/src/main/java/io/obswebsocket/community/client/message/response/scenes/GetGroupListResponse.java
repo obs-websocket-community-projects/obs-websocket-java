7b0aa1556a60f2338459b718c5711894e60c30ad
Pjiesco <Pjiesco@gmail.com>
2022-10-13 18:57:01 +0200

    Add @Getter and @ToString

--- client/src/main/java/io/obswebsocket/community/client/message/response/scenes/GetGroupListResponse.java
+++ client/src/main/java/io/obswebsocket/community/client/message/response/scenes/GetGroupListResponse.java
@@ -4,8 +4,9 @@ import io.obswebsocket.community.client.message.response.RequestResponse;
 import lombok.Getter;
 import lombok.ToString;
 
+@Getter
+@ToString(callSuper = true)
 public class GetGroupListResponse extends RequestResponse<GetGroupListResponse.Data> {
-
   public GetGroupListResponse() {
     super();
   }

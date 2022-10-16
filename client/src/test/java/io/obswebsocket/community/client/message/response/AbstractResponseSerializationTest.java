15464b2a7129672da99acfe1c3b17ca93fc95f00
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2022-10-16 23:18:11 +0200

    core: GetRecordStatus test

--- client/src/test/java/io/obswebsocket/community/client/message/response/AbstractResponseSerializationTest.java
+++ client/src/test/java/io/obswebsocket/community/client/message/response/AbstractResponseSerializationTest.java
@@ -6,7 +6,7 @@ import java.util.function.Consumer;
 public class AbstractResponseSerializationTest extends AbstractSerializationTest {
 
   protected <D, T extends RequestResponse<D>> void assertResponse(String type,
-      Class<T> responseClass, Consumer<D> assertData) {
+      Class<T> responseClass, Consumer<D> assertData) { // TODO: Does not check Request.Type.XXXX
     String jsonFilename = responseClass.getSimpleName();
     String json = this.readResourceFile("responses/" + type + "/" + jsonFilename + ".json");
 
